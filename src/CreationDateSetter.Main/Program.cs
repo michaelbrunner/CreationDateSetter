7cf186f6beffa7e1e1a999dbf4e7b2cf1534152c
Michael Brunner <michael.brunner@axa-winterthur.ch>
2022-02-07 11:22:46 +0100

    Adde parameter to include subfolders

--- src/CreationDateSetter.Main/Program.cs
+++ src/CreationDateSetter.Main/Program.cs
@@ -6,14 +6,39 @@ namespace CreationDateSetter.Main
     {
         public static void Main(string[] args)
         {
-            ProcessDirectory(args[0]);
+            var withSubfolder = false;
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please add a path as argument.");
+                Console.WriteLine("Usage: CreationDateSetter <path> [-s]");
+                return;
+            }
+            
+            if (args.Length == 2)
+            {
+                switch (args[1])
+                {
+                    case "-s":
+                        withSubfolder = true;
+                        break;
+                    default:
+                        Console.WriteLine($"{args[0]} is not valid.");
+                        Console.WriteLine("Usage: CreationDateSetter <path> [-s]");
+                        return;
+                }
+            }
+
+            var path = args[0];
+            
+            ProcessDirectory(path, withSubfolder);
         }
 
-        private static void ProcessDirectory(string path)
+        private static void ProcessDirectory(string path, bool withSubfolder)
         {
             if (Directory.Exists(path))
             {
-                var files = DirectoryParser.ParseDirectory(path);
+                var files = DirectoryParser.ParseDirectory(path, withSubfolder);
                 
                 foreach (var (fileName, absolutePath) in files)
                 {
@@ -38,5 +63,6 @@ namespace CreationDateSetter.Main
             }
 
         }
+
     }
 }
